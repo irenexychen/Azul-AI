d7a87c494809732d55b66944fde57c8bf015b14a
PotentialOnWings <xiangyi99@live.com>
2019-02-11 23:49:36 -0500

    set up of model_train.py

---
+++ model_train.py
@@ -0,0 +1,4 @@
+from model.model import KerasBot, RandomizedKerasBot, IdiotBot
+
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
