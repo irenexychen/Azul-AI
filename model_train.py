8324be4b1a9f97ecc123a05c12c0931c0121c826
MrMiaoMiao <kevin.wkmiao@gmail.com>
2019-05-19 13:28:40 -0400

    Remove pre-existing framework
    Starting automated data generation afresh

--- model_train.py
+++
@@ -1,4 +0,0 @@
-from model.model import KerasBot, RandomizedKerasBot, IdiotBot
-
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Conv2D, MaxPooling2D
