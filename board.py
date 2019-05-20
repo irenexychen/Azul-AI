9097e9d49d6dad5f2cf50fd588a7d2b758770ad8
Irene XiangYi Chen <xiangyi99@live.com>
2019-05-19 20:00:36 -0400

    Completed score calculation logic
    Finished calculation helpers, should be playable

--- board.py
+++ board.py
@@ -23,6 +23,7 @@ class Board:
 	def __init__(self):
 		self.board_container = [[0] * 5,] * 5
 		self.prev_board_state = [[0] * 5,] * 5
+		self.score = 0
 
 	def board_mapping(self, x, y):
 		return self.BOARDMAPPING[x][y]
@@ -54,15 +55,68 @@ class Board:
 		for x in range(5):
 			for y in range(5):
 				if self.board_container[x][y] != self.prev_board_state[x][y]:
+					self.score += self._traverse_row(x,y)
+					self.score += self._traverse_col(x,y)
+		self.save_to_prev_board_state()
+		return self.score
 
- 
 	def calculate_bonus_score(self):
-		for r in self.board_container
+		# full rows bonus
+		num_fullrows = 0
+		for r in self.board_container:
+			for y in range(5):
+				if (r[y] != 1):
+					break
+				else if (y == 4):
+					num_fullrows += 1
+		
+		# full cols bonus
+		num_fullcols = 0
+		for i in range(5):
+			if _traverse_col(0,i) == 5:
+				num_fullcols += 1
+
+		# same type bonus
+		tiletype_tally = {
+				Tile.BLUE: 0,
+				Tile.CHEESE: 0,
+				Tile.FIRE: 0,
+				Tile.BLACK:	0,
+				Tile.SNOW: 0,
+		}
+		for i in range(5):
+			for j in range(5):
+				tiletype_tally[self.board_mapping(i,j)] += 1
+		num_fulltypes = 0
+		for k, v in tiletype_tally:
+			if (v == 5):
+				num_fulltypes += 1
+
+		return num_fullrows * 2 + num_fullcols * 7 + num_fulltypes * 10
 
 	def _traverse_row(self, x, y):
-		score = 0
-		while (x >= 0):
-			score += self.board_container[x][y]
+		score = 1
+		i = x - 1
+		while (i >= 0 and self.board_container[i][y] == 1): 
+			score += self.board_container[i][y]
+			i -= 1
+		i = x + 1
+		while (i <= 4 and self.board_container[i][y] == 1):
+			score += self.board_container[i][y]
+			i += 1
+		return score
 
-		return
-	def _traverse_col(self):
+	def _traverse_col(self, x, y):
+		score = 1
+		j = y - 1
+		while (i >= 0 and self.board_container[x][j] == 1): 
+			score += self.board_container[x][j]
+			j -= 1
+		j = y + 1
+		while (j <= 4 and self.board_container[x][j] == 1):
+			score += self.board_container[x][j]
+			j += 1
+		return score
+
+	def save_to_prev_board_state(self):
+		self.prev_board_state = self.board_container
